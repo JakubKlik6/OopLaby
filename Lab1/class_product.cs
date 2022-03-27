b2f4aabb0d35bb3382e9cb531a3bc7ec6a814520
Jakub Klik <Jakub.klik6@gmail.com>
2022-03-27 16:31:25 +0200

    Create class_product.cs

---
+++ Lab1/class_product.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+    class Produkt
+
+    {
+        private string nazwa;
+        private float cena;
+        private bool wKoszyku;
+
+        Produkt(string a,float b, bool c)
+        {
+            this.nazwa = a;
+            this.cena = b;
+            this.wKoszyku = c;
+        }
+        public override string ToString()
+        {
+            return $"Nazwa: \n {nazwa}" +
+                    $"Cena: ]n {cena}";
+        }
+
+    }
+
+}
