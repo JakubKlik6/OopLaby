cca6f19b790602f19dc2258c326bcce91eda1a45
Jakub Klik <Jakub.klik6@gmail.com>
2022-06-18 15:02:36 +0200

    Create Line_class.cs

---
+++ Lab2/Figure/Line_class.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programowanie
+{
+    class Line : Figure
+    {
+        public Line(double width) : base(width) //dziedziczenie
+        {}
+
+        public override void Draw()
+        {
+            for(int i = 1; i <= this.width; i++)
+            {
+                Console.WriteLine("-");
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+}
